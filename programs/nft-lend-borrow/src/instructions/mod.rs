000feafba2fe2bb635567cd909f9f3d4f9acf6b8
Vivek Sawant <vicky210490@gmail.com>
2024-10-20 18:52:22 +0530

    add borrow module to mod

--- programs/nft-lend-borrow/src/instructions/mod.rs
+++ programs/nft-lend-borrow/src/instructions/mod.rs
@@ -1,7 +1,9 @@
 pub mod create_pool;
 pub mod offer_loan;
 pub mod withdraw_offer;
+pub mod borrow;
 
 pub use create_pool::*;
 pub use offer_loan::*;
 pub use withdraw_offer::*;
+pub use borrow::*;

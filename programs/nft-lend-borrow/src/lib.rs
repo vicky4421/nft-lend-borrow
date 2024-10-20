5f7e6219c62b19e372f0ca88a1b445025284fe7d
Vivek Sawant <vicky210490@gmail.com>
2024-10-20 15:27:57 +0530

    define offer loan instruction in main mod

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -3,6 +3,7 @@ pub mod states;
 pub use states::*;
 
 pub mod instructions;
+pub use instructions::*;
 
 declare_id!("CXViv1YMFLyt4vRPGCjNxCK4UQZQJSfDAXV1J8HA6kzD");
 
@@ -16,5 +17,9 @@ pub mod nft_lend_borrow {
         duration: i64
     ) -> Result<()> {
         instructions::create_pool::handler(ctx, collection_id, duration)
+    }
+
+    pub fn offer_loan(ctx: Context<OfferLoan>, offer_amount: u64) -> Result<()> {
+        instructions::offer_loan::handler(ctx, offer_amount)
     }
 }

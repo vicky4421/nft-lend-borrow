2956437f147d462a5253e54be818afd8549d80d2
Vivek Sawant <vicky210490@gmail.com>
2024-10-20 14:30:58 +0530

    create module for instructions

--- programs/nft-lend-borrow/src/states/offer.rs
+++ programs/nft-lend-borrow/src/states/offer.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 #[account]
 pub struct Offer{
     /// Collection
-    pub collection: Pubkey;
+    pub collection: Pubkey,
 
     /// Offer Amount
     pub offer_lamport_amount: u64,

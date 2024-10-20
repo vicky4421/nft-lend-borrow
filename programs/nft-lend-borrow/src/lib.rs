bd0c2ee523ab9c2d6e647157a2a771c06966a381
Vivek Sawant <vicky210490@gmail.com>
2024-10-20 18:54:55 +0530

    define borrow fn in main mod

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -12,6 +12,8 @@ declare_id!("CXViv1YMFLyt4vRPGCjNxCK4UQZQJSfDAXV1J8HA6kzD");
 
 #[program]
 pub mod nft_lend_borrow {
+    use std::task::Context;
+
     use super::*;
 
     pub fn create_pool(
@@ -24,5 +26,16 @@ pub mod nft_lend_borrow {
 
     pub fn offer_loan(ctx: Context<OfferLoan>, offer_amount: u64) -> Result<()> {
         instructions::offer_loan::handler(ctx, offer_amount)
+    }
+
+    pub fn withdraw_offer(
+        ctx: Context<WithdrawOffer>, 
+        minimum_balance_for_rent_exemption: u64,
+    ) -> Result<()> {
+        instructions::withdraw_offer::handler(ctx, minimum_balanace_for_rent_exemption)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, minimum_balance_for_rent_exemption: u64) -> Result<()> {
+        instructions::borrow::handler(ctx, minimum_balance_for_rent_exemption)
     }
 }

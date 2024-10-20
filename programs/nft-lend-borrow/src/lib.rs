f752f64115f27936578fd754f0496ebeb14641a8
Vivek Sawant <vicky210490@gmail.com>
2024-10-20 14:32:07 +0530

    define function for creating a nft pool in program

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -2,17 +2,19 @@ use anchor_lang::prelude::*;
 pub mod states;
 pub use states::*;
 
+pub mod instructions;
+
 declare_id!("CXViv1YMFLyt4vRPGCjNxCK4UQZQJSfDAXV1J8HA6kzD");
 
 #[program]
 pub mod nft_lend_borrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn create_pool(
+        ctx: Context<CreatePool>, 
+        collection_id: Pubkey, 
+        duration: i64
+    ) -> Result<()> {
+        instructions::create_pool::handler(ctx, collection_id, duration)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}

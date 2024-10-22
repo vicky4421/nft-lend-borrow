f700ca873b84526e4d03ecd20c0994f289fc62fb
Vivek Sawant <vicky210490@gmail.com>
2024-10-22 16:13:18 +0530

    import liquidate to mod

--- programs/nft-lend-borrow/src/instructions/mod.rs
+++ programs/nft-lend-borrow/src/instructions/mod.rs
@@ -3,9 +3,11 @@ pub mod offer_loan;
 pub mod withdraw_offer;
 pub mod borrow;
 pub mod repay;
+pub mod liquidate;
 
 pub use create_pool::*;
 pub use offer_loan::*;
 pub use withdraw_offer::*;
 pub use borrow::*;
 pub use repay::*;
+pub use liquidate::*;
